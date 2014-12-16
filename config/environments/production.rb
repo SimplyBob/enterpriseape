b993216a1340fec0fb34a2bb3aff2a26302b55dd
James Cooper <roverxk9@gmail.com>
2014-12-15 17:47:10 -0800

    adding a few mailer configs to the production deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Enterpriseape::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -77,4 +77,10 @@ Enterpriseape::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Adding for a future mailing feature
+  config.action_mailer.default_url_options = { :host => 'jcoape.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 end
